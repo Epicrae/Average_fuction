4f0cb96a40c01261b5f5a20256ec6a46d2fa6fcf
Epicrae <104163947+Epicrae@users.noreply.github.com>
2023-08-09 13:38:31 -0700

    Update Total Purchase chapter 2-8.cpp
    improvements to line spacing and set precision.

--- Total Purchase chapter 2-8.cpp
+++ Total Purchase chapter 2-8.cpp
@@ -9,13 +9,13 @@ int main() {
 	double item_one = 15.95, item_two = 24.95, item_three = 6.95, item_four = 12.95, item_five = 3.95;
 	double sub_total = item_one + item_two + item_three + item_four + item_five;
 	double sales_tax = 0.07;
-	double total_price = sales_tax * sub_total;
+	double total_price = sub_total* sales_tax;
 	double grand_total = total_price + sub_total;
-	
-	
+
+
 	cout << "Item1=$15.95\nItem2=$24.95\nItem3=$6.95\nItem4=$12.95\nItem5=$3.95\n______________\n"; // prices of items in a list
-	cout << "The sales tax is 7% and your subtotal is "<<grand_total;
-	   
+	cout << "The sales tax is 7% and your subtotal is: ";
+	cout << std::fixed << std::setprecision(2) << "$"<<grand_total << endl;
 
 	system("pause");
 }
