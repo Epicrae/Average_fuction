4602827fe9492611ace512a1ef96a7d308ed70a1
Epicrae <104163947+Epicrae@users.noreply.github.com>
2023-08-09 13:46:52 -0700

    Update avgexamplefuctions.cpp
    cleaned up the code so it would execute.

--- avgexamplefuctions.cpp
+++ avgexamplefuctions.cpp
@@ -6,13 +6,11 @@ using namespace std;
 int sum(int num1, int num2, int num3, int num4, int num5);
 
 int main() {
-		
 
-		
-	cout<< "The avg would be :"<<sum(28,32,37,24,33);
-	return 0;
+	cout << "The avg would be :" << sum(28, 32, 37, 24, 33) << endl;
+
 	system("pause");
-
+	
 }
 
 int sum(int num1, int num2, int num3, int num4, int num5)
@@ -20,23 +18,7 @@ int sum(int num1, int num2, int num3, int num4, int num5)
 	int answer = num1 + num2 + num3 + num4 + num5;
 	int real_answer = answer / 5;
 	return real_answer;
-	
 }
 
- 
- 
- Finding the avg:
-#include <iostream>
-using namespace std;
-#include <iomanip>
 
-
-
-int main()
-{
-	int numbers = (28 + 32 + 37 + 24 + 33) / 5;
-
-	cout << "The avg is :" << numbers << endl;
-	   	 	
-}
 
